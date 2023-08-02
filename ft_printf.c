2260648fbc889a4f4492a8894e3f8155684190e1
rashuakbar <victormf25@outlook.pt>
2023-08-02 12:25:37 +0100

    final version, try 1

--- ft_printf.c
+++ ft_printf.c
@@ -1,6 +1,32 @@
 #include "ft_printf.h"
 
-int	ft_printf(const char *, ...)
+int	check_op(char *string, int *op, va_list args)
 {
+	/*check das opções*/
+}
 
+int	ft_printf(const char *input, ...)
+{
+	int	cont;
+	int	i;
+	char *stg;
+	va_list args;
+
+	stg = (char *)input;
+	cont = 0;
+	i = 0;
+	va_start(args,input);
+	while(stg[i])
+	{
+		if (stg[i] == "%")
+			cont += check_op(stg, &i, args);
+		else
+			{
+				ft_putchar_fd(stg[i],1);
+				cont++;
+			}
+		i++;
+	}
+	va_end(args);
+	return (cont);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -3,7 +3,14 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+# include <stdarg.h>
+
 
 int	ft_printf(const char *, ...);
+int	ft_printf_char(char c);
+int	ft_printf_int(int num);
+int	ft_printf_string(char *src);
+int	ft_printf_pnt(void *pointer);
+
 
 #endif
