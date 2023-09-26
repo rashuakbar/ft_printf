00f5529df969c47b5ccb099dde5fca241f523551
rashuakbar <victormf25@outlook.pt>
2023-09-26 03:31:47 +0100

    hx base funcional

--- ft_printf_hxbase.c
+++ ft_printf_hxbase.c
@@ -4,35 +4,39 @@
 int	count_d(unsigned int n)
 {
 	int	dgt;
-
 	dgt = 0;
+	if (n <= 15 && n >=10)
+		return (1);
 	while (n > 9)
 	{
 		dgt++;
-		n / 10;
+		n = n / 10;
 	}
-	dgt = dgt + 2;
+	dgt = dgt + 1;
 	return (dgt);
 }
 char	*ft_itoa_hx(unsigned int nb, char *base)
 {
 	char	*str;
 	int		i;
-	int		len;
 	int		index;
 
 	if (nb == 0)
 		return (ft_strdup("0"));
-	len = ft_strlen(base);
 	i = count_d(nb);
-	index = 0;
-	str = malloc(sizeof(char) * i);
+	if ((nb > 255 && nb <= 999) || (i <=2))
+		index = 1;
+	else if (i <= 8 )
+		index = 2;
+	else 
+		index = 3;
+	str = malloc(sizeof(char) * i + 1);
 	if(!str)
 		return (NULL);
-	while (nb && index < i)
+	while (nb && index <= i)
 	{
-		str[i-index] = base[nb % len];
-		nb = nb / len;
+		str[i-index] = base[nb % 16];
+		nb = nb / 16;
 		index++;
 	}
 	str[i] = '\0';
@@ -43,24 +47,20 @@ int	ft_printf_hxbase(unsigned int num, char *base)
 {
 	char	*str;
 	int	len;
-
 	str = ft_itoa_hx(num, base);
 	len = ft_strlen(str);
 	ft_putstr_fd(str,1);
 	free(str);
 	return (len);	
 }
-
+/*
 int main(void)
 {
-	int i = 9;
+	int i =31;
 	int c ;
 
 	c = ft_printf_hxbase((unsigned int)i,"0123456789ABCDEF");
-	ft_putchar_fd('\n',1);
-	ft_putchar_fd('a',1);
-	ft_putchar_fd('\n',1);
-	ft_putstr_fd("0123456789ABCDEF\n",1);
-	printf("\n %d", c);
+	printf("\n-%X-",i);
+	printf("\n %d\n", c);
 	return (0);
-}
+}*/
